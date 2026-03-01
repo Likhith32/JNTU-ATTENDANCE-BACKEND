5acd294da557a9f02c1dd950bbb11eb05b2dbadc
Likhith32 <likhithmanakala@gmail.com>
2026-03-01 23:21:45 +0530

    Refactor: Implement final stable version of attendance PDF parsing logic

--- pdf_parser.py
+++ pdf_parser.py
@@ -34,23 +34,27 @@ def parse_attendance_pdf(file_path):
                 continue
 
             # --- Attendance Rows ---
-            date_time_match = re.search(
-                r"(\d{2}-\d{2}-\d{4}).*?(\d{2}:\d{2}).*?(\d{2}:\d{2}).*?(\d{2}:\d{2})",
-                line
-            )
+            date_match = re.search(r"\d{2}-\d{2}-\d{4}", line)
 
-            if date_time_match and current_employee:
-                date_str = date_time_match.group(1)
-                in_time = date_time_match.group(2)
-                out_time = date_time_match.group(3)
-                total_time = date_time_match.group(4)
+            if date_match and current_employee:
+                times = re.findall(r"\d{2}:\d{2}", line)
 
-                current_employee["records"].append({
-                    "date": date_str,
-                    "in": in_time,
-                    "out": out_time,
-                    "total": total_time
-                })
+                if len(times) >= 2:
+                    in_time = times[0]
+
+                    if len(times) == 2:
+                        out_time = times[1]
+                        total_time = "00:00"
+                    else:
+                        out_time = times[-2]
+                        total_time = times[-1]
+
+                    current_employee["records"].append({
+                        "date": date_match.group(),
+                        "in": in_time,
+                        "out": out_time,
+                        "total": total_time
+                    })
 
     return all_data
 
